0adf617960d6169c5e1f143d231ac2f0c70abed9
Jason <hlcheungap@connect.ust.hk>
2025-09-13 13:57:51 +0800

    This is the 3rd commit

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,6 +7,7 @@ public class mainApp1 {
         System.out.println("Program ..Starting...");
         System.out.println("1+1 ="+2);
         System.out.println("Wrong 1+1 ="+3);
+        System.out.println("Very Wrong 1+1 ="+3);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
